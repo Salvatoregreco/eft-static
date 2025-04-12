f8fdacbaaddd90d1ed0bbdad0687ed33f3a0be56
Salvatore Greco <salvatoregreco96@gmail.com>
2025-04-12 11:15:58 +0200

    Initial commit

---
+++ src/alert.js
@@ -0,0 +1,3 @@
+export default function myalert(message) {
+    alert(message);
+}
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './style.css';
+import myalert from './alert.js';
+
+const app = document.getElementById('app');
+
+app.classList.add('container', 'mx-auto', 'p-4');
+
+app.innerHTML = `
+  <h1 class="font-bold text-3xl mb-4">Hello World</h1>
+  <button type="button" class="bg-blue-500 text-white p-2 rounded-md" id="button">Click me</button>
+`;
+
+document.querySelector('#button').addEventListener('click', () => {
+    myalert('Hello World');
+});
